530ba18121a8242d8d3557f115162220025e9185
Mohammed-Afeef-Sayeed <afeefsayeed4.4@gmail.com>
2024-06-05 14:27:53 +0530

    first commit

---
+++ src/components/search/index.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+
+const Search = ({ search, setSearch, handleSearch }) => {
+
+    return (<div className="search-box">
+        <input
+            type="text"
+            className="city-search"
+            placeholder="Enter City Name"
+            name="search"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+        />
+        <button className="search-btn" onClick={handleSearch}>Search Weather</button>
+    </div>)
+}
+
+export default Search
---
+++ src/components/weather/index.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react"
+import Search from "../search"
+
+
+const Weather = () => {
+
+    const [search, setSearch] = useState('')
+    const [load, setLoad] = useState(false)
+    const [data, setData] = useState(null)
+
+    async function fetchWeather(searchCity) {
+        setLoad(true)
+
+        const url = `https://open-weather13.p.rapidapi.com/city/${searchCity}/EN`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'x-rapidapi-key': '6d65dec7e5msh8cea8203d359363p1c9025jsnb4477f47ad05',
+                'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+            if (result) {
+                setData(result)
+                setLoad(false)
+            }
+        } catch (error) {
+            setLoad(false)
+            console.error(error);
+        }
+    }
+
+    function handleSearch() {
+        fetchWeather(search)
+    }
+
+    function getCurrentDate() {
+        return new Date().toLocaleDateString('en-us',{
+            weekday: 'long',
+            month: 'long',
+            day: "numeric",
+            year: 'numeric'
+        })
+    }
+    useEffect(() => {
+        fetchWeather('Hyderabad')
+    }, [])
+
+    console.log(data);
+    return (
+        <div>
+            <Search
+                search={search}
+                setSearch={setSearch}
+                handleSearch={handleSearch}
+
+            />
+            {
+                load ? <div className="loading">Loading ...... </div> :
+                    <div>
+                        <div className="city-name">
+                            <h2>{data?.name}, <span>{data?.sys.country}</span></h2>
+                        </div> 
+                        <div className="date">
+                            <span>{getCurrentDate()}</span>
+                        </div>
+                        <div className="temp">{data?.main?.temp}</div>
+                        <p className="description">
+                            {data && data.weather && data.weather[0] ? data.weather[0].description : ''}
+                        </p>
+                        <div className="weather-info">
+                            <div>
+                                <div>
+                                    <p>Wind Speed</p>
+                                    <p className="wind">{data?.wind?.speed}</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div>
+                                    <p>Humidity</p>
+                                    <p className="humidity">{data?.main?.humidity}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            }
+        </div>
+    )
+}
+export default Weather
